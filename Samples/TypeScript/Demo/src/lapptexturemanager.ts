7ccf51440819ab6e73b43e908825365a1c47106c
L170 <641980694@qq.com>
2020-07-02 22:39:39 +0800

    改bundle

--- Samples/TypeScript/Demo/src/lapptexturemanager.ts
+++ Samples/TypeScript/Demo/src/lapptexturemanager.ts
@@ -70,7 +70,7 @@ export class LAppTextureManager {
 
     // データのオンロードをトリガーにする
     const img = new Image();
-    img.crossOrigin='Anonymous';
+    img.crossOrigin = 'Anonymous';
     img.onload = (): void => {
       // テクスチャオブジェクトの作成
       const tex: WebGLTexture = gl.createTexture();
